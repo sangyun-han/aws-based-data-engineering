0e99d40bcb52f88f8e35db869b12286d122f3893
sangyun-han <sangyun-han@sangyun-hanui-MacBook-Pro.local>
2022-03-05 01:18:08 +0900

    add command line

---
+++ week-1/lecture.sh
@@ -0,0 +1,31 @@
+# download & unzip logstash
+wget https://artifacts.elastic.co/downloads/logstash/logstash-7.16.2-linux-x86_64.tar.gz
+tar -zxvf logstash-7.16.2-linux-x86_64.tar.gz
+
+# download & unzip filebeat
+wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.16.2-linux-x86_64.tar.gz
+tar -zxvf filebeat-7.16.2-linux-x86_64.tar.gz
+
+# run logstash
+cd logstash-7.16.2/ 
+./bin/logstash -f config/logstash-sample.conf
+
+# run filebeat
+cd filebeat-7.16.2-linux-x86_64/
+./filebeat run -e
+
+git clone https://github.com/sangyun-han/aws-based-data-engineering
+cp aws-based-data-engineering/week-1/logstash/logstash-config-* logstash-7.16.2/config/
+
+./logstash-7.16.2/bin/logstash -f config/logstash-config-generator.conf
+./logstash-7.16.2/bin/logstash -f config/logstash-config-file.conf
+./logstash-7.16.2/bin/logstash -f config/logstash-config-filter.conf
+
+
+sudo apt update
+sudo apt install openjdk-11-jdk
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt-get update && sudo apt-get install logstash
