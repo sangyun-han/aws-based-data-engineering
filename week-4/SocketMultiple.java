8c6d63d113ddf1edd4ec241d465a3b8b90449238
lukason-han <lukason.han@kakaocorp.com>
2022-04-02 10:40:59 +0900

    add flink example

---
+++ week-4/SocketMultiple.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.examples.multiple;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
+
+/**
+ * Implements a streaming windowed version of the "WordCount" program.
+ *
+ * <p>This program connects to a server socket and reads strings from the socket. The easiest way to
+ * try this out is to open a text server (at port 12345) using the <i>netcat</i> tool via
+ *
+ * <pre>
+ * nc -l 12345 on Linux or nc -l -p 12345 on Windows
+ * </pre>
+ *
+ * <p>and run this example with the hostname and the port as arguments.
+ */
+@SuppressWarnings("serial")
+public class SocketMultiple {
+
+    public static void main(String[] args) throws Exception {
+
+        // the host and the port to connect to
+        final String hostname;
+        final int port;
+        try {
+            final ParameterTool params = ParameterTool.fromArgs(args);
+            hostname = params.has("hostname") ? params.get("hostname") : "localhost";
+            port = params.getInt("port");
+        } catch (Exception e) {
+            System.err.println(
+                    "No port specified. Please run 'SocketWindowWordCount "
+                            + "--hostname <hostname> --port <port>', where hostname (localhost by default) "
+                            + "and port is the address of the text server");
+            System.err.println(
+                    "To start a simple text server, run 'netcat -l <port>' and "
+                            + "type the input text into the command line");
+            return;
+        }
+
+        // get the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // get input data by connecting to the socket
+        DataStream<String> input = env.socketTextStream(hostname, port, "\n");
+
+        DataStream<String> multiple =
+                input.map(
+                        new MapFunction<String, String>() {
+                            @Override
+                            public String map(String value) throws Exception {
+                                int multiple = Integer.parseInt(value) * 2;
+                                return Integer.toString(multiple);
+                            }
+                        });
+
+        multiple.addSink(new PrintSinkFunction<>()).setParallelism(1);
+
+        env.execute("Socket Window WordCount");
+    }
+}
